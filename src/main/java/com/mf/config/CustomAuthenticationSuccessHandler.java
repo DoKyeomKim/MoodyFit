2425033931877cd6aece2b3a24a09ad0d39bb999
DKLuke <mealn21@naver.com>
2024-07-01 11:17:56 +0900

    로그인시 아이디 기억하기(쿠키) 추가

--- src/main/java/com/mf/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/mf/config/CustomAuthenticationSuccessHandler.java
@@ -2,16 +2,15 @@ package com.mf.config;
 import java.io.IOException;
 import java.util.Collection;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import com.mf.service.CustomUserDetails;
-import com.mf.service.UsersService;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -32,6 +31,25 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
             session.setAttribute("role", authority.getAuthority());
             
         }
+        
+        // 아이디 기억하기 기능 추가(쿠키)
+        String remember = request.getParameter("remember_id");
+        String username = userDetails.getUsername();
+
+        if ("on".equals(remember)) {
+            Cookie rememberCookie = new Cookie("rememberedId", username);
+            // 쿠키 저장 기간 30일 설정
+            rememberCookie.setMaxAge(30 * 24 * 60 * 60);
+            rememberCookie.setPath("/");
+            response.addCookie(rememberCookie);
+        } else {
+        	// 아이디 기억 체크박스 해제 시 쿠키 삭제
+            Cookie rememberCookie = new Cookie("rememberedId", null);
+            rememberCookie.setMaxAge(0);
+            rememberCookie.setPath("/");
+            response.addCookie(rememberCookie);
+        }
+	
         
         response.sendRedirect("/");
     }
