b02593d069fcda30dac292f5aa42dd2c02ae45bb
DKLuke <mealn21@naver.com>
2024-07-01 16:23:23 +0900

    헤더에 전역설정 삭제

--- src/main/java/com/mf/advice/GlobalControllerAdvice.java
+++ src/main/java/com/mf/advice/GlobalControllerAdvice.java
@@ -30,39 +30,9 @@ public class GlobalControllerAdvice {
 	private ProductService productService;
 		
 	    @ModelAttribute
-	    public void categories(Model model,HttpSession session){
+	    public void categories(Model model){
 	    	//카테고리 불러오기
 			List<CategoryDto> category = productService.getCategory();
-			
-			// 등급 불러오기 위해서 시큐리티 인증 확인
-		    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		    if (authentication != null) {
-		        Object principal = authentication.getPrincipal();
-		        Long userIdx = (Long) session.getAttribute("userIdx");
-
-		        // 일반 회원 처리
-		        if (principal instanceof CustomUserDetails) {
-		            CustomUserDetails userDetails = (CustomUserDetails) principal;
-
-		            Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
-		            for (GrantedAuthority authority : authorities) {
-		                if ("ROLE_PERSON".equals(authority.getAuthority())) {
-		                    String levelName = usersService.getLevel(userIdx);
-		                    model.addAttribute("levelName", levelName);
-		                    break;
-		                }
-		            }
-		        } else if (principal instanceof OAuth2User) {
-		            OAuth2User oauth2User = (OAuth2User) principal;
-		            // 사용자의 역할(role)을 확인하여 "ROLE_PERSON"인 경우에만 처리
-		            if (oauth2User.getAuthorities().stream().anyMatch(auth -> "ROLE_PERSON".equals(auth.getAuthority()))) {
-		                // 사용자 등급 정보 가져오기
-		                String levelName = usersService	.getLevel(userIdx);
-		                model.addAttribute("levelName", levelName);
-		            }
-		        }
-		    }
-			
 			
 	        model.addAttribute("category",category);
 	    }
